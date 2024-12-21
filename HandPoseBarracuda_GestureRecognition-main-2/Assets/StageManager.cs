9bf750e4421848a9340e061a2bc493c91a293670
jungwook123 <24sunrin119@sunrint.hs.kr>
2024-12-21 21:51:03 +0900

    스테이지 선택

--- HandPoseBarracuda_GestureRecognition-main-2/Assets/StageManager.cs
+++ HandPoseBarracuda_GestureRecognition-main-2/Assets/StageManager.cs
@@ -3,68 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StageManager : MonoBehaviour
+public class StageManager : Singleton<StageManager>
 {
-    public Button[] buttons;
-    public int curStage = 1;
     
-    private static StageManager instance;
+    private int curStage = 1;
 
-    public StageManager Instance
+    public int CuStage
     {
-        get{return instance;}
+        get { return curStage; }
+        set { curStage = value; }
     }
-    private void Start()
-    {
-        SetButtonName();
-        SetButtonOpacity();
-    }
+    
+    
 
-    public void SetButtonName()
-    {
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            Text buttonText = buttons[i].GetComponentInChildren<Text>();
-
-            buttonText.text = (i + 1).ToString();
-        }
-    }
-
-    public void SetButtonOpacity()
-    {
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            Image buttonImage = buttons[i].GetComponent<Image>();
-
-            Color color = buttonImage.color;
-            if (i < curStage)
-            {
-                buttons[i].interactable = true;
-                color.a = 1.0f;
-                buttonImage.color = color;
-            }
-            else
-            {
-                color.a = 0.2f;
-                buttons[i].interactable = false;
-            }
-
-            Text buttonText = buttons[i].GetComponentInChildren<Text>();
-            if (buttonText != null)
-            {
-                Color textColor = buttonText.color;
-
-                if (i < curStage)
-                {
-                    textColor.a = 1.0f;
-                }
-                else
-                {
-                    textColor.a = 0.2f;
-                }
-
-                buttonText.color = textColor;
-            }
-        }
-    }
 }
